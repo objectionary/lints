ebff58e2d4506b58076137ed171b1b1dd157f98c
h1alexbel <h1alexbelx@gmail.com>
2025-05-02 14:01:02 +0200

    bug(#465): tests adopted

--- src/test/java/org/eolang/lints/ReserveHomeTest.java
+++ src/test/java/org/eolang/lints/ReserveHomeTest.java
@@ -8,7 +8,11 @@ import com.yegor256.tojos.MnCsv;
 import com.yegor256.tojos.TjCached;
 import com.yegor256.tojos.TjDefault;
 import com.yegor256.tojos.TjSynchronized;
+import com.yegor256.tojos.Tojo;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
@@ -23,34 +27,68 @@ import org.junit.jupiter.api.Test;
 final class ReserveHomeTest {
 
     @Test
-    void placesHomeObjectsAsReserved() throws Exception {
+    void placesHomeObjectsAsReserved() {
         final String csv = "target/classes/reserved.csv";
-        new ReserveHome(csv).exec(Paths.get("downloaded", "home").toString());
+        new ReserveHome(csv).exec(ReserveHomeTest.homeLocation());
         MatcherAssert.assertThat(
             "Reserved objects are empty, but they should not",
-            new TjCached(
-                new TjSynchronized(
-                    new TjDefault(
-                        new MnCsv(csv)
-                    )
-                )
-            ).select(tojo -> true),
-            Matchers.iterableWithSize(Matchers.greaterThan(0))
+            ReserveHomeTest.reserved(csv),
+            Matchers.aMapWithSize(Matchers.greaterThan(0))
         );
     }
 
     @Test
-    void placesHomeObjectsWithCorrectNames() throws Exception {
-//        MatcherAssert.assertThat(
-//            "Home objects do not match with expected format",
-//            new ReserveHome().value().values(),
-//            Matchers.everyItem(
-//                Matchers.hasToString(
-//                    Matchers.matchesRegex(
-//                        "org\\.eolang(?:\\.[a-zA-Z_][a-zA-Z0-9_-]*)+\\.eo"
-//                    )
-//                )
-//            )
-//        );
+    void placesHomeObjectsWithCorrectNames() {
+        final String csv = "target/classes/reserved.csv";
+        new ReserveHome(csv).exec(ReserveHomeTest.homeLocation());
+        MatcherAssert.assertThat(
+            "Home objects do not match with expected format",
+            ReserveHomeTest.reserved(csv).values(),
+            Matchers.everyItem(
+                Matchers.hasToString(
+                    Matchers.matchesRegex(
+                        "org\\.eolang(?:\\.[a-zA-Z_][a-zA-Z0-9_-]*)+\\.eo"
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * Fetch all reserved objects.
+     *
+     * @param path Path with reserved objects
+     * @return Map of reserved objects
+     */
+    private static Map<String, String> reserved(final String path) {
+        final List<Tojo> selected = new TjCached(
+            new TjSynchronized(
+                new TjDefault(
+                    new MnCsv(path)
+                )
+            )
+        ).select(tojo -> true);
+        System.out.println(selected);
+        final Map<String, String> collected = selected.stream()
+            .map(Tojo::toMap)
+            .flatMap(map -> map.entrySet().stream())
+            .collect(
+                Collectors.toMap(
+                    Map.Entry::getKey,
+                    Map.Entry::getValue,
+                    (v1, v2) -> v2
+                )
+            );
+        System.out.println(collected);
+        return collected;
+    }
+
+    /**
+     * Home location.
+     *
+     * @return Home objects path as string
+     */
+    private static String homeLocation() {
+        return Paths.get("downloaded", "home").toString();
     }
 }
