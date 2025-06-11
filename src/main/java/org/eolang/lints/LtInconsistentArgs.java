29e90deb3a6ea194426b3114c358693b59b36025
h1alexbel <h1alexbelx@gmail.com>
2025-06-11 17:23:04 +0300

    bug(#624): clean for qulice

--- src/main/java/org/eolang/lints/LtInconsistentArgs.java
+++ src/main/java/org/eolang/lints/LtInconsistentArgs.java
@@ -10,7 +10,6 @@ import com.jcabi.xml.XMLDocument;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/test/java/org/eolang/lints/VoidXpathTest.java
+++ src/test/java/org/eolang/lints/VoidXpathTest.java
@@ -21,7 +21,7 @@ final class VoidXpathTest {
         {
             "main.$.x.∅, //o[@name='main']/o[@base='$.x']",
             "foo.foo.foo.$.x.∅, //o[@name='foo']/o[@name='foo']/o[@name='foo']/o[@base='$.x']",
-            "@.foo.:anonymous.$.x.∅, //o[@name='@']/o[@name='foo']/o/o[@base='$.x']",
+            "@.foo.:anonymous.$.x.∅, //o[@name='@']/o[@name='foo']/o/o[@base='$.x']"
         }
     )
     void convertsToXpath(final String fqn, final String xpath) {
