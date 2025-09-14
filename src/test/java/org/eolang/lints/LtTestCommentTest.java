f0f72b8c8eac81c6b602ccf41513326235e4e1b6
KISLAY KUMAR <135631873+kislayykumar@users.noreply.github.com>
2025-09-14 17:57:51 +0530

    Update LtTestCommentTest.java
    Signed-off-by: KISLAY KUMAR <135631873+kislayykumar@users.noreply.github.com>

--- src/test/java/org/eolang/lints/LtTestCommentTest.java
+++ src/test/java/org/eolang/lints/LtTestCommentTest.java
@@ -1,34 +1,36 @@
 /*
-
-SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
-SPDX-License-Identifier: MIT
-*/
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
 package org.eolang.lints;
 
-import com.github.lombrozo.xnav.Xnav;
 import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.Collection;
 
+/**
+ * Test for {@link LtTestComment}.
+ *
+ * @since 0.0.1
+ */
 final class LtTestCommentTest {
 
     @Test
     void reportsNoDefectsWhenNoCommentPresent() throws IOException {
-        XML xml = new XMLDocument(
-                "<program>" +
-                        "<object name='foo' line='10'>" +
-                        "<o name='+test' line='12'>" +
-                        "</o>" +
-                        "</object>" +
-                        "</program>"
+        final XML xml = new XMLDocument(
+            "<program>"
+                + "<object name='foo' line='10'>"
+                + "<o name='+test' line='12'>"
+                + "</o>"
+                + "</object>"
+                + "</program>"
         );
-        LtTestComment lint = new LtTestComment();
-        Collection<Defect> defects = lint.defects(xml);
+        final LtTestComment lint = new LtTestComment();
+        final Collection<Defect> defects = lint.defects(xml);
         MatcherAssert.assertThat(
             "Should not report defects when test object has no comment",
             defects,
@@ -38,16 +40,16 @@ final class LtTestCommentTest {
 
     @Test
     void reportsNoDefectsWhenNoObjectPresent() throws IOException {
-        XML xml = new XMLDocument(
-                "<program>" +
-                        "<object name='foo' line='10'>" +
-                        "<o name='bar' line='12'>" +
-                        "</o>" +
-                        "</object>" +
-                        "</program>"
+        final XML xml = new XMLDocument(
+            "<program>"
+                + "<object name='foo' line='10'>"
+                + "<o name='bar' line='12'>"
+                + "</o>"
+                + "</object>"
+                + "</program>"
         );
-        LtTestComment lint = new LtTestComment();
-        Collection<Defect> defects = lint.defects(xml);
+        final LtTestComment lint = new LtTestComment();
+        final Collection<Defect> defects = lint.defects(xml);
         MatcherAssert.assertThat(
             "Should not report defects when no test object is present",
             defects,
@@ -57,21 +59,21 @@ final class LtTestCommentTest {
 
     @Test
     void containsGuidanceInMotive() throws IOException {
-        LtTestComment lint = new LtTestComment();
+        final LtTestComment lint = new LtTestComment();
         MatcherAssert.assertThat(
-                "Motive must discourage comments in test objects",
-                lint.motive(),
-                Matchers.containsString("Comments in test objects are discouraged")
+            "Motive must discourage comments in test objects",
+            lint.motive(),
+            Matchers.containsString("Comments in test objects are discouraged")
         );
     }
 
     @Test
     void returnsStableId() {
-        LtTestComment lint = new LtTestComment();
+        final LtTestComment lint = new LtTestComment();
         MatcherAssert.assertThat(
-                "Rule id must be 'test-has-comment'",
-                lint.name(),
-                Matchers.equalTo("test-has-comment")
+            "Rule id must be 'test-has-comment'",
+            lint.name(),
+            Matchers.equalTo("test-has-comment")
         );
     }
 
