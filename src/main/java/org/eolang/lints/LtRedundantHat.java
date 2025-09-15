b1880edf236f53234db4fe48452c7261f7f53692
SNEHA KB <63995915+Snehakb1219-christ@users.noreply.github.com>
2025-09-15 12:14:03 +0530

    Update LtRedundantHat.java
    Signed-off-by: SNEHA KB <63995915+Snehakb1219-christ@users.noreply.github.com>

--- src/main/java/org/eolang/lints/LtRedundantHat.java
+++ src/main/java/org/eolang/lints/LtRedundantHat.java
@@ -15,19 +15,22 @@ import org.eolang.parser.OnDefault;
 
 /**
  * Lint that warns if a redundant {@code ^} is used.
- *
- * @since 0.0.60
+ * @since 0.0.59
  */
 final class LtRedundantHat implements Lint<XML> {
-
     @Override
     public Collection<Defect> defects(final XML xmir) throws IOException {
         final Collection<Defect> defects = new ArrayList<>(0);
         final Xnav xml = new Xnav(xmir.inner());
-        final List<Xnav> objects = xml.path("//o[@base='^']").collect(Collectors.toList());
+        final List<Xnav> objects = xml
+            .path("//o[@base='^']")
+            .collect(Collectors.toList());
         for (final Xnav object : objects) {
             final String name = object.attribute("name").text().get();
-            final List<Xnav> parents = object.path(String.format("ancestor::o[.//o[@name='%s']]", name))
+            final List<Xnav> parents = object
+                .path(
+                    String.format("ancestor::o[.//o[@name='%s']]", name)
+                )
                 .collect(Collectors.toList());
             if (parents.isEmpty()) {
                 defects.add(
@@ -49,7 +52,7 @@ final class LtRedundantHat implements Lint<XML> {
 
     @Override
     public String motive() throws IOException {
-        return "The '^' notation is used when there are no naming conflicts, please omitted for brevity.";
+        return "The '^' notation is redundant and can be omitted for brevity.";
     }
 
     @Override
@@ -57,4 +60,3 @@ final class LtRedundantHat implements Lint<XML> {
         return "redundant-hat";
     }
 }
-
