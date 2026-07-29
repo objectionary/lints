650fd002cc292c255a97a4799d3b033a0a247b81
Ilya Vdovin <qwxlyx@gmail.com>
2026-07-30 00:42:25 +0300

    Fix qulice formatting: fluent calls with braces on same line (#1106)

--- src/main/java/org/eolang/lints/LtReservedName.java
+++ src/main/java/org/eolang/lints/LtReservedName.java
@@ -45,26 +45,21 @@ final class LtReservedName implements Lint {
 
     @Override
     public Collection<Defect> defects(final XML xmir) throws IOException {
-        return new Xnav(xmir.inner())
-            .path("//o[@name]")
-            .filter(
-                object -> {
-                    final String name = object.attribute("name").text().get();
-                    return !isLibraryName(name) && this.reserved.containsKey(name);
-                }
-            )
-            .map(
-                object -> new Defect.Default(
-                    this.name(),
-                    Severity.WARNING,
-                    new LineOf(object).value(),
-                    String.format(
-                        "Object name \"%s\" is already reserved by object in the \"%s\"",
-                        object.attribute("name").text().get(),
-                        this.reserved.get(object.attribute("name").text().get())
-                    )
+        return new Xnav(xmir.inner()).path("//o[@name]")
+            .filter(object -> {
+                final String name = object.attribute("name").text().get();
+                return !isLibraryName(name) && this.reserved.containsKey(name);
+            })
+            .map(object -> new Defect.Default(
+                this.name(),
+                Severity.WARNING,
+                new LineOf(object).value(),
+                String.format(
+                    "Object name \"%s\" is already reserved by object in the \"%s\"",
+                    object.attribute("name").text().get(),
+                    this.reserved.get(object.attribute("name").text().get())
                 )
-            )
+            ))
             .collect(Collectors.toList());
     }
 
